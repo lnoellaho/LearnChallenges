9d183b2b9d48697022e502cd65eff67dfab3d3d1
learn <learn@learn12.local>
2017-11-30 15:01:43 -0800

    Day 4 car challenge

---
+++ Week 1/Day 4/carObject.js
@@ -0,0 +1,110 @@
+//Week 1 Day 4 Leeann and Erika
+/*
+var myCar = {
+  year: 2018,
+  make: "Lotus",
+  color: "matte black",
+  getData: function() {
+    return "I have a " + this.year + " " + this.make + " in " + this.color },
+
+   };
+
+myCar.getData();
+
+/* creating first car and calling getData
+var car1 = createCarObject(2018, "Lotus", "yellow")
+
+car1.getData()
+*/
+
+
+
+//story 1 and story 2
+function createCarObject(modelYear, carMake, carColor) {
+  return {
+    year: modelYear,
+    make: carMake,
+    color: carColor,
+    getData: function() {
+      return "I have a " + this.year + " " + this.make + " in " + this.color
+    },
+    carSpeed: function() {
+      var speed = 200;
+      return {
+        getSpeed: function() {return speed; },
+        accelerate: function() {speed + 10;},
+        brake: function() {speed -7}
+      };
+    }
+  };
+}
+/* creating first car and calling getData
+var car1 = createCarObject(2018, "Lotus", "yellow")
+
+call car1.carSpeed().getSpeed()
+200
+*/
+
+//Story 3
+/*
+attempt #1
+function createCarObject(modelYear, carMake, carColor) {
+  return {
+    year: modelYear,
+    make: carMake,
+    color: carColor,
+    getData: function() {
+      return "I have a " + this.year + " " + this.make + " in " + this.color
+    },
+    carSpeed: function() {
+      var speed = 50; //must be moved out of carSpeed. Everytime we call carSpeed, speed will reset to 50.
+      return {
+        getSpeed: function() {return speed; },
+        accelerate: function() {
+            while (speed <= 70) {
+                var newSpeed =speed + 10;
+                speed = newSpeed;
+                return speed}}, //return will end loop
+        brake: function() {speed -7}
+      };
+    }
+  };
+}
+
+*/
+//correct solution
+function createCarObject(modelYear, carMake, carColor) {
+    var speed = 50;
+  return {
+    year: modelYear,
+    make: carMake,
+    color: carColor,
+    getData: function() {
+      return "I have a " + this.year + " " + this.make + " in " + this.color
+    },
+    carSpeed: function() {
+      return {
+        getSpeed: function() {return speed; },
+        accelerate: function() {
+            while (speed < 70) {
+                var newSpeed = speed + 10;
+                speed = newSpeed;
+                console.log(speed);
+            }},
+        brake: function() {
+            while (speed >= 7) {
+                var newSpeed = speed - 7;
+                speed = newSpeed;
+                console.log(speed);
+            }
+            while (speed > 0) {
+                var newSpeed = speed -1;
+                speed = newSpeed;
+                console.log(speed);
+            }}
+      };
+    }
+  };
+}
+
+//Story 4
