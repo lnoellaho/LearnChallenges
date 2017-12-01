7935b817d0d10fd91a7fd97a3d35b8172324130e
learn <learn@learn03.local>
2017-12-01 13:58:02 -0800

    iterations

---
+++ Week 1/Day 5/iterations_2.js
@@ -0,0 +1,24 @@
+for (var i=0; i<10; i++) { console.log(i); }
+
+while (i < 10) {
+    i++;
+    console.log(i)
+
+    var i = 0;
+while (i < 10) {
+    i++;
+    console.log (i);
+}
+
+for (var a=10; a >= 0; a--)
+{ console.log(a);
+}
+
+var oneArray = [1, 2, 3];
+var twoArray = [2, 30, 1];
+
+var mergeArray = oneArray.concat(twoArray);
+
+mergeArray.sort()
+
+mergeArray.splice(1, 2)
